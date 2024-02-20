05bbe12dee56808b0aa89cab6bbb3a54c079abd3
Błażej Domagała <petitoff@outlook.com>
2024-02-20 09:50:50 +0100

    Add Counter component unit tests

---
+++ src/Counter/Counter.test.js
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Counter } from "./Counter";
+
+describe("Counter", () => {
+  it("should render the count", () => {
+    render(<Counter />);
+    const elementText = screen.getByText("Count: 0");
+    expect(elementText).toBeInTheDocument();
+  });
+
+  it("should increment the count", () => {
+    render(<Counter />);
+    const button = screen.getByRole("button", { name: "Increment" });
+    fireEvent.click(button);
+
+    const elementText = screen.getByText("Count: 1");
+    expect(elementText).toBeInTheDocument();
+  });
+});
